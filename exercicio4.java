9503f450559a08eda4318e6b9a2c172e3ee6c61c
Paulo César Rocha <pcrrp.pereira@gmail.com>
2025-04-25 23:51:08 -0300

    Add files via upload

--- exercicio4.java
+++ exercicio4.java
@@ -1,26 +1,42 @@
 import java.util.Scanner;
-public class exercicio4 {
+
+public class exercicio4{
     public static void main(String[] args){
         Scanner entrada = new Scanner(System.in);
-        double numero1;
-        double numero2;
-        
-        System.out.println("Digite o primeiro número ");
-            numero1= entrada.nextDouble();
-        System.out.println("Digite o segundo número ");
-            numero2= entrada.nextDouble();
-            
-        if(numero1 == numero2){
-        System.out.println("Os números são iguais ");
-    }
-        else if(numero1 > numero2){
-            System.out.println(numero1 + "=>" + numero2);
-        }
-        else{
-            System.out.println(numero2 + "=>" + numero1);
+        double salario;
+        double bonus;
+        double novoSalario;
+
+        System.out.println("Digite o seu salário: ");
+        salario = entrada.nextDouble();
+
+        System.err.println("Digite seu plano (A, B, C): ");
+        char plano= entrada.next().charAt(0);    
+
+        switch(plano){
+            case 'A':
+            bonus = salario * 0.10;
+            break;
+
+            case 'B':
+            bonus = salario * 0.15;
+            break;
+
+            case 'C':
+            bonus = salario * 0.20;
+            break;
+
+            default:
+            System.out.println("Plano inválido ");
+            entrada.close();
+            return;
         }
 
-        entrada.close();    
+        novoSalario = salario + bonus;
+            System.out.println("Seu aumento foi de: " + bonus);
+            System.out.println("Seu salário agora é: " + novoSalario);
+
+        entrada.close();
+
     }
 }
-
