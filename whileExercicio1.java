15f9fa305a71a0ee783548cb5aa7735ced4c563d
Paulo César Rocha <pcrrp.pereira@gmail.com>
2025-05-25 20:33:09 -0300

    Add files via upload

---
+++ whileExercicio1.java
@@ -0,0 +1,11 @@
+public class exercicio1{
+    public static void main(String[] args){
+        int contador;
+        contador = 100;
+
+        while(contador <= 200){
+            System.out.println(contador + "");
+            contador++;
+        }
+    }
+}
