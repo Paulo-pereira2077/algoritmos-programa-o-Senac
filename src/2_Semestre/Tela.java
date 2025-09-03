e0a47165e253ea9e3b17d67d7e70be9a8d3ef4f1
Paulo César Rocha <paulo.crrpereira@gmail.com>
2025-09-03 14:42:27 -0300

    Aprendendo o funcionamento de classe e como aplicar junto com função

---
+++ src/2_Semestre/Tela.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Tela {
+    Scanner entrada = new Scanner (System.in);
+
+public void apresentacao(){
+    System.out.println("------ Exercício 3 (Calculando média do aluno ------");
+    System.out.println();
+}
+
+public double pedirNotas(String mensagem){
+
+    System.out.println(mensagem);
+    return entrada.nextDouble();
+}
+
+public void exibirSituacao(double calculoMedia){
+
+    System.out.println("A média é: " + calculoMedia);
+    System.out.println(calculoMedia);
+}
+
+}
---
+++ src/2_Semestre/exercicioClasse.java
@@ -0,0 +1,25 @@
+
+public class exercicioClasse {
+    
+public static void main(String[] args) {
+
+    Tela tela = new Tela();
+    Boletim boletim = new Boletim();
+
+    // exibe a logo do programa
+   tela.apresentacao();
+
+// recebe as notaas
+double nota1 = tela.pedirNotas("Digite sua 1º nota:");
+
+double nota2 = tela.pedirNotas("Digite sua 2º nota:");
+
+double nota3 = tela.pedirNotas("Digite sua 3º nota:");
+
+    double calculoMedia = boletim.calculando(nota1, nota2, nota3);
+
+    boletim.verificandoSituacao(calculoMedia);
+
+}
+
+}
