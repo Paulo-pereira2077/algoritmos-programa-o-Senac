8935d587d129d0405078083b34598dff96d2c7d1
Paulo César Rocha <paulo.crrpereira@gmail.com>
2025-09-04 17:23:51 -0300

    Praticando laço de repetição na main

--- src/2_Semestre/normal.java
+++ src/2_Semestre/normal.java
@@ -1,3 +1,36 @@
+import java.util.Scanner;
+
 public class normal {
     
+    static Scanner entrada = new Scanner(System.in);
+    public static void main(String[] args) {
+        
+        int quantidade;
+        String tipoIngresso;
+        double valorIngresso = 28.5;
+
+        System.out.println("==== Cinema ====");
+        System.out.println();// pula uma linha
+
+        System.out.println("Digite a quantidade de ingressos: ");
+        quantidade = entrada.nextInt();
+
+        System.out.println("Digite o tipo do ingrsso (Inteira ou meia): ");
+        tipoIngresso = entrada.next();
+
+        double valor;
+        if (tipoIngresso.equalsIgnoreCase("Inteira")){            
+            valor = valorIngresso * quantidade;
+            System.out.println("Valor total: " + valor);
+        }
+        else if (tipoIngresso.equalsIgnoreCase("Meia")){
+            valor = (valorIngresso / 2) * quantidade;
+            System.out.println("Valor total: " + valor);
+        }
+        else {
+            System.out.println("Esse tipo não existente");
+        }
+
+        entrada.close();
+    }
 }
