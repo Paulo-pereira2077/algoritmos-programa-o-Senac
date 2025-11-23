09fe3eb0bcd8ef2687e40ecf7ecd6ac510dff68e
Paulo <pcrrp.pereira@gmail.com>
2025-11-23 17:18:19 -0300

    Exercício 1

--- src/2_Semestre/exercicio_classe_colecao_uml/exercicio1/App.java
+++ src/2_Semestre/exercicio_classe_colecao_uml/exercicio1/App.java
@@ -1,12 +1,56 @@
 import java.util.Scanner;
 
 public class App {
-
     public static void main(String[] args) {
 
-        Scanner entrada = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        Blacklist blacklist = new Blacklist();
+        int opcao = 0;
 
-        
+        do {
+            Menu.exibirMenu();
+            System.out.print("Escolha uma opção: ");
+            opcao = sc.nextInt();
+            sc.nextLine(); 
 
+            switch (opcao) {
+                case 1:
+                    System.out.print("Digite o nome para adicionar: ");
+                    String nomeAdd = sc.nextLine();
+                    blacklist.adicionar(nomeAdd);
+                    break;
+
+                case 2:
+                    System.out.print("Digite a posição que deseja alterar: ");
+                    int posAlterar = sc.nextInt();
+                    sc.nextLine();
+
+                    System.out.print("Digite o novo nome: ");
+                    String novoNome = sc.nextLine();
+
+                    blacklist.alterar(posAlterar, novoNome);
+                    break;
+
+                case 3:
+                    blacklist.listar();
+                    break;
+
+                case 4:
+                    System.out.print("Digite a posição para remover: ");
+                    int posRemover = sc.nextInt();
+                    blacklist.remover(posRemover);
+                    break;
+
+                case 5:
+                    System.out.println("Encerrando...");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida!");
+            }
+
+        } while (opcao != 5);
+
+        sc.close();
     }
 }
