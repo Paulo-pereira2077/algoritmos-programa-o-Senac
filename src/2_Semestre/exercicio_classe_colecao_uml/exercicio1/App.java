39105221c5d1c3ead048201592c4239aa8eab8c8
Paulo <pcrrp.pereira@gmail.com>
2025-11-23 21:40:07 -0300

    Exercício 5

--- src/2_Semestre/exercicio_classe_colecao_uml/exercicio1/App.java
+++ src/2_Semestre/exercicio_classe_colecao_uml/exercicio1/App.java
@@ -1,72 +1,49 @@
 import java.util.Scanner;
 
 public class App {
-
-    static Scanner entrada = new Scanner(System.in);
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        ControleReservas controle = new ControleReservas();
 
-        GerenciadorTarefas ger = new GerenciadorTarefas();
-
-        int opcao = 0;
+        int opcao;
 
         do {
             System.out.println("===== MENU =====");
-            System.out.println("1. Nova tarefa");
-            System.out.println("2. Listar tarefas");
-            System.out.println("3. Remover tarefa");
-            System.out.println("4. Alterar tarefa");
-            System.out.println("5. Marcar como concluída");
-            System.out.println("6. Sair");
+            System.out.println("1. Nova reserva");
+            System.out.println("2. Listar reservas");
+            System.out.println("3. Chamar próximo");
+            System.out.println("0. Sair");
             System.out.println("================");
 
             System.out.print("Escolha uma opção: ");
-            opcao = entrada.nextInt();
-            entrada.nextLine();
+            opcao = sc.nextInt();
+            sc.nextLine();
 
             switch (opcao) {
 
                 case 1:
-                    System.out.print("Código da tarefa: ");
-                    String cod = entrada.nextLine();
+                    System.out.print("Nome do responsável: ");
+                    String resp = sc.nextLine();
 
-                    System.out.print("Descrição da tarefa: ");
-                    String desc = entrada.nextLine();
+                    System.out.print("Quantidade de lugares: ");
+                    int lugares = sc.nextInt();
 
-                    ger.novaTarefa(new Tarefa(cod, desc));
+                    controle.novaReserva(resp, lugares);
                     break;
 
                 case 2:
-                    ger.listar();
+                    controle.listar();
                     break;
 
                 case 3:
-                    System.out.print("Posição da tarefa para remover: ");
-                    int posR = entrada.nextInt();
-                    ger.remover(posR);
+                    Reserva r = controle.proximo();
+                    if (r != null) {
+                        System.out.println("Chamando: " + r.getResponsavel() + " - " + r.getLugares() + " lugares");
+                    }
                     break;
 
-                case 4:
-                    System.out.print("Posição da tarefa para alterar: ");
-                    int posA = entrada.nextInt();
-                    entrada.nextLine();
-
-                    System.out.print("Novo código: ");
-                    String newCod = entrada.nextLine();
-
-                    System.out.print("Nova descrição: ");
-                    String newDesc = entrada.nextLine();
-
-                    ger.alterar(posA, newCod, newDesc);
-                    break;
-
-                case 5:
-                    System.out.print("Posição da tarefa para concluir: ");
-                    int posC = entrada.nextInt();
-                    ger.concluir(posC);
-                    break;
-
-                case 6:
+                case 0:
                     System.out.println("Encerrando...");
                     break;
 
@@ -74,8 +51,8 @@ public class App {
                     System.out.println("Opção inválida!");
             }
 
-        } while (opcao != 6);
+        } while (opcao != 0);
 
-        entrada.close();
+        sc.close();
     }
 }
